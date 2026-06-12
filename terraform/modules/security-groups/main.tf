072dda39295d78d8effb18cbeb3758fe790b6c5f
sharma-ns2025 <sharma.ns2025@gmail.com>
2026-06-12 21:50:46 +0530

    Backend Code Fix 9

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -108,4 +108,20 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_security_group" "ecs_sg" {
   name   = "ecs-sg"
   vpc_id = var.vpc_id
+
+  # Allow inbound traffic from the ALB
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  # Allow all outbound traffic (Required to reach ECR, S3, Secrets Manager, etc.)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- terraform/modules/vpc-endpoints/main.tf
+++ terraform/modules/vpc-endpoints/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "vpc_endpoints_sg" {
     # Allow HTTPS traffic from anywhere within the VPC
     #cidr_blocks = ["10.0.0.0/16"] 
     
-    security_groups = [var.ecs_security_group_id]
+    security_groups = [var.ecs_security_group_id, var.security_group_id]
   }
   
   egress {
